3c3c6805adda25eef51d7407159fbbb0bb03c6a7
unknown <lightbreaker812@gmail.com>
2024-10-21 20:39:49 +0630

    Updated a little

---
+++ src/basics/basicmath.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class basicmath {
+	 
+	 public static void main(String []args){
+		int a=10, b=13 , c=30 ,d=21;
+		int summ=a+b;
+		System.out.println("Hello");
+		System.out.print("a+b="+summ);
+	}
+}
